2625ce79591bc8f55ad338ed2d39eef78b86d45b
srimathimk77 <srimathimk77@gmail.com>
2026-06-08 01:13:09 -0700

    Delete src directory

--- src/lib/voter.ts
+++
@@ -1,8 +0,0 @@
-export function getVoterId(): string {
-  let id = localStorage.getItem("voter_id");
-  if (!id) {
-    id = crypto.randomUUID();
-    localStorage.setItem("voter_id", id);
-  }
-  return id;
-}
